2828b44f30a2d03265e0ec4674db88e219b0bfcb
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-04-29 23:18:09 +0200

    Updated GetRange method.

--- List.cs
+++ List.cs
@@ -105,5 +105,12 @@ public class List<T>
             throw new ArgumentOutOfRangeException(nameof(count), "Count is less than 0.");
         }
 
+
+        if (this._count - index < count)
+        {
+            throw new ArgumentException("Invalid range.");
+        }
+
+
     }
 }
