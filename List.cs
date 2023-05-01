f11de8a7d6309479ae84bf57d56714ad4d7a2ed6
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-01 22:43:13 +0200

    Updated List.cs file.

--- List.cs
+++ List.cs
@@ -87,7 +87,10 @@ public class List<T>
 
     public IEnumerator<T> GetEnumerator()
     {
-
+        for (int i = 0; i < this._count; i++)
+        {
+            yield return _items[i];
+        }
     }
 
     public List<T> GetRange(int index, int count)
