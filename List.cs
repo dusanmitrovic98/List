c366ea1f54df713a4eaf21818a0d32925cfc0c0c
dusanmitrovic98 <dusanmitrovic@elfak.rs>
2023-05-01 22:36:25 +0200

    Updated List.cs file.

--- List.cs
+++ List.cs
@@ -5,4 +5,10 @@ public class List<T>
     private T[] _items;
     private int _count;
     private const int DEFAULT_CAPACITY = 4;
+
+    public List()
+    {
+        this._items = new T[DEFAULT_CAPACITY];
+        this._count = 0;
+    }
 }
